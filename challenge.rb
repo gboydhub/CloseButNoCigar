4873199d9736e4a545b4e00207e598f5eebfdb9b
Eponick <eponickstream@gmail.com>
2018-06-11 16:04:20 -0400

    Get match count/pass multi

--- challenge.rb
+++ challenge.rb
@@ -11,5 +11,14 @@ def exact_match(str1, str2)
 end
 
 def get_number_matches(str1, str2)
-    4
+    counter = 0
+    match_count = 0
+    while counter < str1.length
+        if str1[counter] == str2[counter] then
+            match_count += 1
+        end
+        counter += 1
+    end
+
+    return match_count
 end
